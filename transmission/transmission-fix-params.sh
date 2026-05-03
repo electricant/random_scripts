3a86621b615575d029844dc9b56ddf5e7ea2485a
Electric Ant <electricant@anche.no>
2026-05-03 17:04:12 +0200

    Script rewritten + added a --dry-run command line switch

--- transmission/transmission-fix-params.sh
+++ transmission/transmission-fix-params.sh
@@ -9,7 +9,20 @@
 #
 
 # Authentication user&password is passed on the cli (keep it secret!)
-AUTH="$1"
+AUTH="${1:-}"
+
+if [[ -z "$AUTH" ]]; then
+    echo "Missing auth (user:password)"
+    exit 1
+fi
+
+# Support an optional --dry-run switch to prevent destructive changes
+DRY_RUN=0
+
+if [[ "${2:-}" == "--dry-run" ]]; then
+    DRY_RUN=1
+fi
+
 # This scrpt will match torrents in this directory (supports globbing)
 MATCH_DIR='*/distro-iso'
 # Set seed ratio for matched torrents to this value
@@ -22,42 +35,10 @@ AUTOREMOVE_DATE=$(date +%s -d "6 months ago")
 # Function to list all available (active) torrents by id
 # For details about awk matching see:
 #	https://www.math.utah.edu/docs/info/gawk_5.html#SEC28
-# TODO: ignore finished torrents. How do they appear on this list?
 function list_active_ids {
-	transmission-remote --auth $AUTH -l | \
-		awk '$1 ~ /[0-9]+/{print $1}'
+	transmission-remote --auth "$AUTH" -l | \
+		awk 'NR>1 && $1 ~ /^[0-9]+$/ {print $1}'
 }
-
-# Get the data directory for the specified torrent id
-function get_data_dir {
-	transmission-remote --auth $AUTH --torrent $1 --info | \
-		awk '/Location:/ {print $2}'
-}
-
-# Get the name for the specified torrent id
-function get_name {
-	transmission-remote --auth $AUTH --torrent $1 --info | \
-		awk '/Name:/ {print $2}'
-}
-
-# Get the current seed ratio for the specified torrent id
-function get_cur_ratio {
-	transmission-remote --auth $AUTH --torrent $1 --info | \
-		awk '/Ratio:/ {print $2}'
-}
-
-# Get the target seed ratio for the specified torrent id
-function get_target_ratio {
-	transmission-remote --auth $AUTH --torrent $1 --info | \
-		awk '/Ratio Limit:/ {print $3}'
-}
-
-# Get the time when the torrent was added as unix epoch
-function get_added_epoch {
-	date +%s -d "$(transmission-remote --auth $AUTH -t $1 --info | \
-		awk '/Date added:/ {print $3,$4,$5,$6,$7}')"
-}
-
 # Set the seed ratio of a certain torrent id to the declared value
 function set_seed_ratio {
 	transmission-remote --auth $AUTH --torrent $1 --seedratio $2
@@ -69,22 +50,47 @@ function set_priority {
 }
 
 # Main
-for id in `list_active_ids`; do
+for id in $(list_active_ids); do
+	info=$(transmission-remote --auth "$AUTH" -t "$id" --info)
+
+	name=$(echo "$info" | awk -F': ' '/Name:/ {print $2}')
+	location=$(echo "$info" | awk -F': ' '/Location:/ {print $2}')
+	added=$(echo "$info" | awk -F': ' '/Date added:/ {print $2}')
+
+	if [[ -z "$added" ]]; then
+		echo "Skipping ID $id (no date found)"
+		continue
+	fi
+
+	added_epoch=$(date +%s -d "$added")
+
 	echo ""
-	echo "Processing $(get_name $id) (ID: $id)..."
-	if [[ $(get_data_dir $id) = $MATCH_DIR ]]; then
-		echo "Data directory matched." \
-			"Current ratio: $(get_cur_ratio $id)/$(get_target_ratio $id)" 
-		
-		if [ $(get_added_epoch $id) -lt $AUTOREMOVE_DATE ]
-		then
-			echo "Torrent too old: removed."
-			transmission-remote --auth $AUTH -t $id --remove-and-delete
+	echo "Processing $name (ID: $id)..."
+
+	if [[ "$location" == $MATCH_DIR ]]; then
+		echo "Matched directory: $location"
+
+		if [[ "$added_epoch" -lt "$AUTOREMOVE_DATE" ]]; then
+			if [[ "$DRY_RUN" -eq 1 ]]; then
+				echo "[DRY] Would remove torrent: $name"
+			else
+				echo "Removing torrent: $name"
+				transmission-remote --auth "$AUTH" -t "$id" \
+					--remove-and-delete
+			fi
 		else
-			set_seed_ratio $id $TARGET_RATIO
-			set_priority $id $PRIORITY
+			if [[ "$DRY_RUN" -eq 1 ]]; then
+				echo "[DRY] Would set ratio=$TARGET_RATIO and" \
+					"priority=$PRIORITY"
+			else
+				transmission-remote --auth "$AUTH" -t "$id" \
+					--seedratio "$TARGET_RATIO"
+				transmission-remote --auth "$AUTH" -t "$id" \
+					--bandwidth-"$PRIORITY"
+			fi
 		fi
 	fi
 done
-echo DONE.
+
+echo "DONE"
 
