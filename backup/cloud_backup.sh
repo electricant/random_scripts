55cbf1f05c54f5bf19431bd86718eb805595ed45
electricant <electricant@anche.no>
2026-09-07 14:24:09 +0000

    Fied source directories

--- backup/cloud_backup.sh
+++ backup/cloud_backup.sh
@@ -27,8 +27,8 @@ BACKUP_MAX_SIZE_GB=90
 # produces b/a/foo, whereas this command:
 #     rsync -a a/ b
 # produces b/foo.
-SOURCE_DIRS=("/mnt/data/archive" "/mnt/data/Foto" "/mnt/data/expenseowl"
-             "/mnt/data/radicale-data" "/mnt/data/syncthing")
+SOURCE_DIRS=("/mnt/data/archive" "/mnt/data/Foto" "/mnt/data/radicale-data"
+             "/mnt/data/syncthing" "/mnt/data/www")
 
 # Remote destination where the backup will be stored
 REMOTE="nas_backup@opc.scaramuzza.me:~/data"
